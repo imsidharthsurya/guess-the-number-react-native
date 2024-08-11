00aa7f6c51b00132be389f33b5ad4d8e819222e1
Sidharth Surya <imsidharthsurya@gmail.com>
2024-08-11 17:48:26 +0530

    reset & confirm number

--- components/StartGameScreen.js
+++ components/StartGameScreen.js
@@ -1,40 +1,79 @@
-import React from "react";
-import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import Card from "./Card";
 import Colors from "../constants/colors";
 import Input from "./Input";
 const StartGameScreen = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+  const handleInputChange = (inputText) => {
+    //replacing any char. other than 0-9 with empty string
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
+  const handleReset = () => {
+    setEnteredValue("");
+    setConfirmed(false);
+  };
+  const handleConfirm = () => {
+    const num = parseInt(selectedNumber);
+    if (num === NaN || num <= 0 || num > 99) {
+      //ie. invalid number entered
+      return;
+    }
+    setEnteredValue("");
+    setConfirmed(true);
+    setSelectedNumber(enteredValue);
+    //order doesn't matter bc it'll happen in batch
+  };
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game!</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Enter a Number</Text>
-        <Input
-          style={styles.input}
-          blurOnSubmit={true}
-          autoCapitalize="none"
-          autoCorrect={false}
-          keyboardType="numeric"
-          maxLength={2}
-        />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button
-              title="Reset"
-              onPress={() => {}}
-              color={Colors.secondaryColor}
-            />
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Enter a Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit={true}
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="numeric"
+            maxLength={2}
+            onChangeText={handleInputChange}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title="Reset"
+                onPress={handleReset}
+                color={Colors.secondaryColor}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Confirm"
+                onPress={handleConfirm}
+                color={Colors.primaryColor}
+              />
+            </View>
           </View>
-          <View style={styles.button}>
-            <Button
-              title="Confirm"
-              onPress={() => {}}
-              color={Colors.primaryColor}
-            />
-          </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+        {confirmed && <Text>Chosen number: {selectedNumber}</Text>}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 const styles = StyleSheet.create({
